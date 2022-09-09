d3643dc1de9af2b3960e2dde9338c263960f0dc0
Matheus Barbosa <matheuskleber@live.com>
2022-09-09 18:27:23 -0300

    chore(deps): adiciona dependencias

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import "bootstrap-datepicker/dist/js/bootstrap-datepicker.min";
+import "select2/dist/js/select2.min";
+// import "bootstrap-datepicker/dist/locales/bootstrap-datepicker.pt-BR.min";
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-window._ = require('lodash');
+window._ = require("lodash");
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -7,13 +7,14 @@ window._ = require('lodash');
  */
 
 try {
-    window.Popper = require('@popperjs/core').default;
-    window.$ = window.jQuery = require('jquery');
+    window.Popper = require("@popperjs/core").default;
+    window.$ = window.jQuery = require("jquery");
+    window.dt = require("datatables.net");
 
     window.Swal = require("sweetalert2/dist/sweetalert2.min").default;
 
-    window.bootstrap = require('bootstrap');
-} catch (e) { }
+    window.bootstrap = require("bootstrap");
+} catch (e) {}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -21,9 +22,9 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+window.axios = require("axios");
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
